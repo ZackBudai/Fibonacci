08dde763b1f5e86654f5c9cb0c1d4e730c0b5dc8
ZackBudai <64515138+ZackBudai@users.noreply.github.com>
2022-12-22 18:04:02 +1000

    Revised Version

--- 2VariableFibonacci.py
+++ 2VariableFibonacci.py
@@ -1,17 +1,11 @@
-'''
-Demonstration of a version of the Fibonacci
-algorithm that I came up with
-that uses only two variables
-excluding the input variable
-'''
-n1 = 0
-n2 = 1
+def fib(n):
+  a, b = 0, 1
+  for i in range(n):
+    print(a)
+    a, b = b, a + b
 
-## Note: this does not display the initial
-## 0 and 1 in the sequence but can be
-## easily adjusted if needed
+
 termNumber = int(input("Please enter the number of terms you would like to print in the Fibonacci sequence."))
-for i in range(0,termNumber):
-	print(n1+n2)
-	n2 = n1+n2
-	n1 = n2-n1
+
+fib(termNumber)
+
